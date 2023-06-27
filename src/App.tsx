be4e82654dab3d66aa2e47ed7a29d3c12bd0327e
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2023-06-27 11:25:41 +0400

    display map on the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useRef } from "react";
+import MapView from "@arcgis/core/views/MapView";
+import Map from "@arcgis/core/Map";
+
 import "./App.css";
 
 function App() {
-  return <h1>Hello Arcgis SDK</h1>;
+  const mapRef = useRef(null);
+  useEffect(() => {
+    if (!mapRef?.current) return;
+
+    const map = new Map({
+      basemap: "osm",
+    });
+    const view = new MapView({
+      map: map,
+      container: mapRef.current,
+      center: [55, 25],
+      zoom: 13,
+    });
+    return () => view && view.destroy();
+  }, []);
+  return <div className="viewDiv" ref={mapRef}></div>;
 }
 
 export default App;
