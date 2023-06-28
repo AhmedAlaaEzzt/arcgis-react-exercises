6db762e4102714a5a5492f8849a73f886594df77
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2023-06-28 11:35:04 +0400

    Render a marker on the map

--- src/ArcGIS-Components/ArcMapView/ArcMapView.tsx
+++ src/ArcGIS-Components/ArcMapView/ArcMapView.tsx
@@ -1,5 +1,10 @@
 import { useEffect, useRef } from "react";
 
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer.js";
+import Graphic from "@arcgis/core/Graphic";
+import Point from "@arcgis/core/geometry/Point";
+import SimpleMarkerSymbol from "@arcgis/core/symbols/SimpleMarkerSymbol";
+
 import { createMapView } from "../../ArcGIS-SDK";
 
 import "./ArcMapView.css";
@@ -10,6 +15,24 @@ export const ArcMapView = () => {
     if (!mapRef?.current) return;
 
     const view = createMapView(mapRef.current);
+    const graphicsLayer = new GraphicsLayer();
+    view.map.add(graphicsLayer);
+
+    const point = new Point({
+      longitude: 55,
+      latitude: 25,
+    });
+
+    const simpleMarkerSymbol = new SimpleMarkerSymbol({
+      color: "red",
+    });
+
+    const graphicPoint = new Graphic({
+      geometry: point,
+      symbol: simpleMarkerSymbol,
+    });
+
+    graphicsLayer.add(graphicPoint);
 
     return () => view && view.destroy();
   }, []);
