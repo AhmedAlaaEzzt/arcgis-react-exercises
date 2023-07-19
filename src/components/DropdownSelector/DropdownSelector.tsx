bac1fe778c7305ee8d250232aa854b52e57e20b9
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2023-07-19 07:12:17 +0400

    print out the change of the dropdown

--- src/components/DropdownSelector/DropdownSelector.tsx
+++ src/components/DropdownSelector/DropdownSelector.tsx
@@ -11,7 +11,11 @@ function DropdownSelector({ defaultValue, options }: IDropdownSelectorProps) {
       className="position-absolute bottom-0 start-0 mx-3 my-3"
       style={{ zIndex: 1000 }}
     >
-      <Form.Select>
+      <Form.Select
+        onChange={(e) => {
+          console.log(e.target.value);
+        }}
+      >
         {defaultValue && <option>{defaultValue}</option>}
         {options.map((option) => (
           <option key={option.value} value={option.value}>
